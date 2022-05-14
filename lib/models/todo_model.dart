4a514352390a32cefc29385fcf815bdc673ef550
Delarram <Delurram@gmail.com>
2022-05-14 20:32:27 +0630

    changed container padding and added floating action button

--- lib/models/todo_model.dart
+++ lib/models/todo_model.dart
@@ -1,21 +1,27 @@
-
 import 'package:easyapproach/models/taskmodel.dart';
 import 'package:flutter/cupertino.dart';
 
-class TodoModel extends ChangeNotifier{
-  int index = 0;
-  List<TaskModel> taskList = [];
+class TodoModel extends ChangeNotifier {
 
-  addTaskInList(){
-    TaskModel taskModel = TaskModel(title: taskList.length.toString(),detail: taskList.length.toString(),icon: taskList.length.toString());
+  List<TaskModel> taskList = [];
+      int index = 0;
+  addTaskInList() {
+    TaskModel taskModel = TaskModel(
+        title: taskList.length.toString(),
+        detail: taskList.length.toString(),
+        icon: taskList.length.toString());
     taskList.add(taskModel);
     notifyListeners();
   }
 
- // void reset(){
-   // TaskModel taskModel = TaskModel(title: taskList.length.toString(),detail: taskList.length.toString(),icon: taskList.length.toString());
-     //taskList.clear();
-    //notifyListeners();
-//  }
+   reset() {
+    // TaskModel taskModel = TaskModel(title: taskList.length.toString(),detail: taskList.length.toString(),icon: taskList.length.toString());
+    taskList.clear();
+    notifyListeners();
+  }
+   remove(){
+   taskList.remove("");
+    notifyListeners();
+    }
 
 }
--- lib/view/my_home_page.dart
+++ lib/view/my_home_page.dart
@@ -13,105 +13,154 @@ class MyHomePage extends StatelessWidget {
         elevation: 0,
         backgroundColor: Colors.lightBlue[900],
         centerTitle: true,
-        title: const Text("TodoModel",style: TextStyle(fontSize: 16,fontFamily: "roboto"),
+        title: const Text(
+          "TodoModel",
+          style: TextStyle(fontSize: 16, fontFamily: "roboto"),
         ),
         leading: IconButton(
           onPressed: () {},
           icon: const Icon(Icons.menu),
         ),
       ),
-      body: ListView(
-        scrollDirection: Axis.vertical,
+      body: Column(
         children: [
-          Column(
+          SizedBox(
+            width: double.infinity,
+            height: 120,
+            child: Column(
               crossAxisAlignment: CrossAxisAlignment.center,
-
-            children: const <Widget> [
-              SizedBox(height: 20,),
-              Text("12 : 00 Pm",
-                style:TextStyle(fontSize: 45,fontWeight: FontWeight.bold,color: Colors.white54),),
-              Text("Current time",
-                style:TextStyle(fontSize: 18,fontWeight: FontWeight.bold,color: Colors.white54),
-              ),
-            ],
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const <Widget>[
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  "12 : 00 Pm",
+                  style: TextStyle(
+                      fontSize: 45,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white54),
+                ),
+                Text(
+                  "Current time",
+                  style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white54),
+                ),
+              ],
+            ),
           ),
           const SizedBox(
             height: 30,
           ),
-          Container(
-            height: 700,
-           decoration: const BoxDecoration(
-             color: Colors.white,
-             borderRadius: BorderRadius.only(topLeft:
-             Radius.circular(50),topRight: Radius.circular(50)
-             )
-           ),
-             child: Consumer<TodoModel>
-               (builder: ((context, todo, child) =>
-                ListView.builder(
-                  scrollDirection: Axis.vertical,
-                    shrinkWrap: true,
-                    itemCount: todo.taskList.length,
-                    itemBuilder:(context,index)=>
-                        Padding(
-                          padding: const EdgeInsets.all(15.0),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-
-                            children: [
-                              CircleAvatar(
-                                backgroundImage:  AssetImage("assets/earth.png"),backgroundColor: Colors.transparent,
-                                radius: 35,
-
-
-                                onBackgroundImageError: (e, s) {
-                                  debugPrint('image issue, $e,$s');
-                                },
-                              ),
-                          SizedBox(width: 30,),
-                          Expanded(
-                            child: ListTile(
-                              title: Text("This is todo ${todo.taskList[index].title}",style: const TextStyle(fontSize: 15,fontWeight: FontWeight.bold),
-                              ),
-                              subtitle: Text("This is todo ${todo.taskList[index].detail}",style: const TextStyle(fontSize: 15,fontWeight: FontWeight.bold),
-                              ),
-                              trailing: Icon(Icons.check_circle, color: Colors.greenAccent,),
+          Expanded(
+            child: Container(
+              width: double.infinity,
+              height: double.infinity,
+              decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(50),
+                      topRight: Radius.circular(50))),
+              child: Consumer<TodoModel>(
+                builder: ((context, todo, child) => Padding(
+                  padding: const EdgeInsets.only(top: 60),
+                  child: ListView.builder(
+                      scrollDirection: Axis.vertical,
+                      shrinkWrap: true,
+                      padding: EdgeInsets.only(bottom: 100),
+                      itemCount: todo.taskList.length,
+                      itemBuilder: (context, index) => Padding(
+                            padding: const EdgeInsets.all(15.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                CircleAvatar(
+                                  backgroundImage: AssetImage("assets/earth.png"),
+                                  backgroundColor: Colors.transparent,
+                                  radius: 35,
+                                  onBackgroundImageError: (e, s) {
+                                    debugPrint('image issue, $e,$s');
+                                  },
+                                ),
+                                SizedBox(
+                                  width: 30,
+                                ),
+                                Expanded(
+                                  child: ListTile(
+                                    title: Text(
+                                      "This is todo ${todo.taskList[index].title}",
+                                      style: const TextStyle(
+                                          fontSize: 15,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    subtitle: Text(
+                                      "This is todo ${todo.taskList[index].detail}",
+                                      style: const TextStyle(
+                                          fontSize: 15,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    trailing: Icon(
+                                      Icons.check_circle,
+                                      color: Colors.greenAccent,
+                                    ),
+                                  ),
+                                ),
+                              ],
                             ),
-                          ),
-                            ],
-                          ),
-                        )
-                )
-             ),
-             ),
+                          )),
+                )),
+              ),
             ),
-
+          ),
         ],
       ),
-    //  floatingActionButton: FloatingActionButton(
-      //  child: Icon(Icons.add),
-        //onPressed: (){
-          //Provider.of<TodoModel>(context,listen: false).addTaskInList();
-        //},
-      //),
-    //  floatingActionButton: Row(
-      //  mainAxisAlignment: MainAxisAlignment.end,
-        //children: [
-         // FloatingActionButton(onPressed: (){
-           // Provider.of<TodoModel>(context,listen: false).reset();
-          //},
-          //backgroundColor: Colors.lightBlue[900],
-            //child: Icon(Icons.exposure_zero_rounded,color: Colors.white,size: 30,),
-          //),
-          //const SizedBox(width: 30,),
-          //FloatingActionButton(onPressed: (){
-            //Provider.of<TodoModel>(context,listen: false).addTaskInList();
-          //},
-          //backgroundColor: Colors.lightBlue[900],
-          //child:  Icon(Icons.add,color: Colors.white,size: 30,),
-          //)
-        //],
-      //),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () {
+              Provider.of<TodoModel>(context, listen: false).reset();
+            },
+            backgroundColor: Colors.lightBlue[900],
+            child: Icon(
+              Icons.exposure_zero_rounded,
+              color: Colors.white,
+              size: 30,
+            ),
+          ),
+          const SizedBox(
+            width: 20,
+          ),
+          FloatingActionButton(
+            onPressed: () {
+              Provider.of<TodoModel>(context, listen: false).addTaskInList();
+            },
+            backgroundColor: Colors.lightBlue[900],
+            child: Icon(
+              Icons.add,
+              color: Colors.white,
+              size: 30,
+            ),
+          ),
+          const SizedBox(
+            width: 20,
+          ),
+          FloatingActionButton(
+            onPressed: () {
+              Provider.of<TodoModel>(context, listen: false).remove();
+              print("hello");
+            },
+            backgroundColor: Colors.lightBlue[900],
+            child: Icon(
+              Icons.remove,
+              color: Colors.white,
+              size: 30,
+            ),
+          )
+        ],
+      ),
     );
   }
 }
